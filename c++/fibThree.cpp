ddbe81a8000f38271de59769d5e599deb1d1a934
ensj <ensjtrans@gmail.com>
2021-08-20 12:35:02 -0700

    update comments

--- c++/fibThree.cpp
+++ c++/fibThree.cpp
@@ -5,16 +5,18 @@ int main() {
   mpz_t two;
   mpz_set_ui(two, 2);
 
-  std::cout << "[";
-  bool firstEntry = true;
+  std::cout << "["; //Formatting
+  bool firstEntry = true; //Variable only exists for formatting
 
   for (size_t i = 0; i <= 1000000; ++i)
   {
     mpz_t first, sec, product, res, exp;
-    mpz_set_ui(first, ((10 * i) + 7));
+
+    //This block generates all pairs that are 7 and 9 mod 10. 
+    mpz_set_ui(first, ((10 * i) + 7)); 
     mpz_set_ui(sec, ((10 * i) + 9));
   
-    if (mpz_probab_prime_p(first, 2) && mpz_probab_prime_p(sec, 2))
+    if (mpz_probab_prime_p(first, 2) && mpz_probab_prime_p(sec, 2)) //If both numbers are prime:
     {
       mpz_init(product);
       mpz_init(res);
@@ -22,7 +24,6 @@ int main() {
 
       mpz_mul(product, first, sec);
       mpz_sub_ui(exp, product, 1);
-      //gmp_printf("%Zd, %Zd: ", exp, product);
       mpz_powm(res, two, exp, product);
 
       if(!mpz_cmp_ui(res, 1))
@@ -33,7 +34,7 @@ int main() {
 
       else
       {
-        if(firstEntry)
+        if(firstEntry)  //If statement is only for formatting
         {
           gmp_printf("[%d, %Zd]", ((10 * i) + 7), res);
           firstEntry = false;
@@ -45,7 +46,7 @@ int main() {
     }
   }
 
-  std::cout << "]" << std::endl;
+  std::cout << "]" << std::endl; //Formatting
 
   return EXIT_SUCCESS;
 }
